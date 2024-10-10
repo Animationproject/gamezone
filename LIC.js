18a0fe688ab447a61c85f5afa44bfe8cd37df2ad
Animationproject <animationsproject719@gmail.com>
2024-10-10 21:47:45 +0530

    Android
    my second gamezone website for free

---
+++ LIC.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('play-now').addEventListener('click', function() {
+    alert('Redirecting to game page...');
+    window.location.href = "https://www.freefire.com";
+});
